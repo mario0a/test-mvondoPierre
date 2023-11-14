820dd7c81322568779193c1bc05b1e838b1f7bd1
mario0a <mariusmvondo@gmail.com>
2023-11-14 15:50:24 +0100

    Affichage des carte de jeu ok

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\JeuRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    #[Route('/', name: 'app_main')]
+    public function index(JeuRepository $jeuRepository): Response
     {
-        return $this->render('main/index.html.twig');
+        return $this->render('main/index.html.twig',[
+            'jeu'=>$jeuRepository->findAll(),
+        ]);
     }
 }
